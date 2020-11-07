bc48286962022919b145e16b33341b998196c816
Aaron <48164620+aaronsang@users.noreply.github.com>
2020-11-06 16:59:28 -0800

    Add files via upload

--- udemy-100-days-of-python/1-band-name-generator.py
+++ udemy-100-days-of-python/1-band-name-generator.py
@@ -2,7 +2,6 @@
 Generates a band name for the user
 """
 
-print('Welcome to your very own band name generator.')
-city = input('What city did you grow up in? \n')
+city = input('Welcome to your very own band name generator. \nWhat city did you grow up in? \n')
 pet = input("What was your favorite pet's name? \n")
 print(f'Thank you! Your band name should be: {pet + " " + city}')
